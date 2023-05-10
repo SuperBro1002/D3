5b8b14d7077e3d4c1f41bdd79922d432a8e01f24
SuperBro1002 <126834262+SuperBro1002@users.noreply.github.com>
2023-05-10 14:16:22 -0700

    Added code for physics

--- level1.js
+++ level1.js
@@ -14,8 +14,8 @@ class First extends Phaser.Scene {
         this.h = this.game.config.height;
         this.s = this.game.config.width * 0.01;
 
-        // this.cameras.main.setBackgroundColor('#444');
-        // this.cameras.main.fadeIn(this.transitionDuration, 0, 0, 0);
+         this.cameras.main.setBackgroundColor('#444');
+         this.cameras.main.fadeIn(this.transitionDuration, 0, 0, 0);
 
         // this.add.rectangle(this.w * 0.75, 0, this.w * 0.25, this.h).setOrigin(0, 0).setFillStyle(0);
         // this.add.text(this.w * 0.75 + this.s, this.s)
@@ -47,6 +47,53 @@ class First extends Phaser.Scene {
         this.add.rectangle(1000, 100, 150, 150, 0xffff75);
         // let player = this.add.text(this.w * 0.3, this.w * 0.3, "👾")
         //     .setFontSize(this.s * 2);
+
+        this.matter.world.setBounds();
+
+        this.matter.add.mouseSpring();
+
+        const group = this.matter.world.nextGroup(true);
+
+        const bridge = this.matter.add.stack(160, 290, 15, 1, 0, 0, (x, y) => Phaser.Physics.Matter.Matter.Bodies.rectangle(x - 20, y, 53, 20, {
+            collisionFilter: { group: group },
+            chamfer: 5,
+            density: 0.005,
+            frictionAir: 0.05
+        }));
+        
+        this.matter.add.chain(bridge, 0.3, 0, -0.3, 0, {
+            stiffness: 1,
+            length: 0,
+            render: {
+                visible: false
+            }
+        });
+        
+        const stack = this.matter.add.stack(250, 50, 6, 3, 0, 0, (x, y) => Phaser.Physics.Matter.Matter.Bodies.rectangle(x, y, 50, 50, Phaser.Math.Between(20, 40)));
+
+        this.matter.add.rectangle(30, 490, 220, 380, {
+            isStatic: true,
+            chamfer: { radius: 20 }
+        }),
+
+        this.matter.add.rectangle(770, 490, 220, 380, {
+            isStatic: true,
+            chamfer: { radius: 20 }
+        }),
+
+        this.matter.add.worldConstraint(bridge.bodies[0], 2, 0.9, {
+            pointA: { x: 140, y: 300 },
+            pointB: { x: -25, y: 0 }
+        });
+
+        this.matter.add.worldConstraint(bridge.bodies[bridge.bodies.length - 1], 2, 0.9, {
+            pointA: { x: 660, y: 300 },
+            pointB: { x: 25, y: 0 }
+        });
+
+
+
+
     }
 
 
@@ -59,6 +106,16 @@ const game = new Phaser.Game({
         autoCenter: Phaser.Scale.CENTER_BOTH,
         width: 1920,
         height: 1080,
+    },
+    physics: {
+        default: 'matter',
+        matter: {
+            gravity: {
+                y:0.8
+            },
+            debug: true,
+            debugBodyColor: 0xffffff
+        }
     },
     scene: [First,],
     backgroundColor: 0x000000,
